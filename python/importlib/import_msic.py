1eddbffcc39bfc08ce361f0c3eb6e9c01e1a391c
Shihongguang <2590032200@qq.com>
2018-07-26 17:41:37 +0800

    fix

--- python/importlib/import_msic.py
+++ python/importlib/import_msic.py
@@ -1,9 +1,13 @@
+import sys
+import os
+
 from importlib import import_module
 
 
 __all__ = ["import_module", "import_class"]
 
-def import_class(model_class):
+def import_class(model_class:str):
+
     _import_model_, _import_class_ = model_class.rsplit(".", 1)
     _import_model_ = import_module(_import_model_)
 
@@ -13,3 +17,20 @@ def import_class(model_class):
         raise ImportError(model_class)
 
     return _import_class_
+
+
+def import_file(filepath):
+    abspath = os.path.abspath(filepath)
+    dirname, file = os.path.split(abspath)
+    fname, fext = os.path.splitext(file)
+
+    if fext != '.py':
+        raise ValueError("Not a Python source file: %s" % abspath)
+    if dirname:
+        sys.path = [dirname] + sys.path
+    try:
+        module = import_module(fname)
+    finally:
+        if dirname:
+            sys.path.pop(0)
+    return module
