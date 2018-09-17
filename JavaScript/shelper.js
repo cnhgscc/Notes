a085e3e11396db5d96d3b30a5632d2b61772f79c
Shihongguang <2590032200@qq.com>
2018-09-17 19:44:16 +0800

    fix

--- JavaScript/shelper.js
+++ JavaScript/shelper.js
@@ -4,5 +4,10 @@ $shelper = {
         var p = arguments[1] || ""
         var defaultValue = arguments[2] || ""
         return obj[p] || defaultValue
+    },
+    Math: {
+        DF2Percent: function(a, b){
+            return (a/b*100).toFixed((arguments[2] || 2)) + (arguments[3] || '%')
+        }
     }
 }
